53cab5b402474ee6e46f52a4cdcf7dc1c8d93ddd
maksymzav <maksym.zavorotynskyi@shopping-feed.com>
2022-03-13 18:59:39 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rch-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'refactoring-challenge';
+}
